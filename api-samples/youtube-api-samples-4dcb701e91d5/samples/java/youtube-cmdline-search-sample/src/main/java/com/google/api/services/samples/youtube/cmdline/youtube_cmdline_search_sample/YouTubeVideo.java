4a07caad8374bf8325ef0248c30e78f36e4bcd55
chad <chad@Link>
2014-07-10 10:55:26 -0600

    added the search source files to the project

---
+++ api-samples/youtube-api-samples-4dcb701e91d5/samples/java/youtube-cmdline-search-sample/src/main/java/com/google/api/services/samples/youtube/cmdline/youtube_cmdline_search_sample/YouTubeVideo.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.google.api.services.samples.youtube.cmdline.youtube_cmdline_search_sample;
+
+/**
+ *
+ * @author chad
+ */
+public class YouTubeVideo {
+    private String title;
+    private String id;
+    private String thumb;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getThumb() {
+        return thumb;
+    }
+
+    public void setThumb(String URL) {
+        this.thumb = URL;
+    }
+    
+    
+}
